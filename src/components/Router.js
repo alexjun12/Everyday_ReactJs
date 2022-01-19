7b3ccc95beb65821c305a6b115aeaabe04e5b55d
alexjun12 <alexjun12@naver.com>
2022-01-20 00:58:16 +0900

    22_01_20study

---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+import {HashRouter as Router, Route, Switch} from "react-router-dom"
+
+export default () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <Router>
+            <Switch>
+                {isLoggedIn ? (
+                <>
+                <Route exact path = "/">
+                    <Home />
+                </Route>
+                </>  
+                ) : (
+                <Route exact path = "/">
+                     <Auth /> 
+                </Route>
+                )}
+            </Switch>
+        </Router>
+    );
+}
