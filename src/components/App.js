a69574123bb7c5ec6317e6b6fdc18284e4999123
alexjun12 <alexjun12@naver.com>
2022-01-25 23:22:57 +0900

    22_01_25commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import AppRouter from "components/Router";
-import { useState } from "react"; 
+import { useEffect, useState } from "react"; 
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setIsLoggedIn(true);
+      }else{
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[]);
+  setInterval(() => {
+
+  },2000);
   return (
   <>
-    <AppRouter isLoggedIn ={isLoggedIn} />
+    {init ? <AppRouter isLoggedIn ={isLoggedIn} /> : "Initializing..."}
     <footer>&copy; Firebase_WebTest {new Date().getFullYear()}</footer>
   </>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,4 +1,15 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
-const Profile = () => <span>Profile</span>;
+function Profile() {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    return (
+        <button onClick={onLogOutClick}>Log Out</button>
+    );
+}
 export default Profile;
