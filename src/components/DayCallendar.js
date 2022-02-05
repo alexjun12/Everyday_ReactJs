fb928388dd6e377f26b28cb40ab318c5be16a818
alexjun12 <alexjun12@naver.com>
2022-02-06 04:02:35 +0900

    22_02_06

--- src/components/DayCallendar.js
+++ src/components/DayCallendar.js
@@ -1,13 +1,45 @@
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import { ko } from "date-fns/esm/locale";
-import "react-datepicker/dist/react-datepicker.css";
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 function DayCallendar({day}){
-    const [startDate, setStartDate] = useState(new Date());
+    const events = [
+        {
+          id: 1,
+          title: 'event 1',
+          start: '2021-06-14T10:00:00',
+          end: '2021-06-14T12:00:00',
+        },
+        {
+          id: 2,
+          title: 'event 2',
+          start: '2021-06-16T13:00:00',
+          end: '2021-06-16T18:00:00',
+        },
+        { id: 3, title: 'event 3', start: '2021-06-17', end: '2021-06-20' },
+      ];
 
     return(
-        <DatePicker locale = { ko } selected={startDate} onChange={(date) => setStartDate(date)} inline />
+        <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+            center: 'dayGridMonth,timeGridWeek,timeGridDay new',
+        }}
+        customButtons={{
+            new: {
+              text: 'new',
+              click: () => alert('new event'),
+            },
+        }}
+        events = {events}
+        eventColor="red"
+        nowIndicator
+        dateClick={(e) => console.log(e.dateStr)}
+        eventClick={(e) => console.log(e.event.id)}
+      />
 );}
 
 export default DayCallendar;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,8 +4,6 @@ import DayCal from "components/DayCallendar";
 import "style.css";
 
 function Home(){    
-    let date = new Date();
-  
     const friends = [];
     for(let i = 1; i < 8; i++){
       friends.push(<button className="friendsBt"></button>);
@@ -14,11 +12,6 @@ function Home(){
     return(
         <div>
             <h1 className="mTitle">EveryDay</h1>
-            <h2 className="textCenter">
-              <button>◀</button>
-              {date.getFullYear()}년{date.getMonth() + 1}월
-              <button>▶</button>
-            </h2>
             <Link to = "/profile">
                 <button>My Profile</button>
             </Link>
