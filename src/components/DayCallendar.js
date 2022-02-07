7ddd0c235941a601669cffb92d4a1a9e982e76ee
alexjun12 <alexjun12@naver.com>
2022-02-07 17:17:46 +0900

    22_02_07

--- src/components/DayCallendar.js
+++ src/components/DayCallendar.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
+import moment, { relativeTimeRounding } from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import Modal from 'react-modal';
@@ -9,27 +9,54 @@ function DayCallendar(){
   moment.locale('ko-KR');
   const localizer = momentLocalizer(moment);
   const [ModalIsOpen, setModalIsOpen] = useState(false);
-
-    const events = [
-        {
-          title: 'event 1',
-          start: '2021-06-14T10:00:00',
-          end: '2021-06-14T12:00:00',
-        },
-    ];
+  const [events,addEvent] = useState([]);
+  let getYear;
+  let getMonth;
+  let getDay;
+    const addSchedule = (sy,sm,sd) => {
+        const sched = {
+          id : 0,
+          title : 'aasd',
+          allday : true,
+          start : new Date(sy,sm,sd),
+          end: new Date(sy,sm,sd),
+        };
+        addEvent(events.concat(sched));
+    }
     return(
       <div>
       <Calendar
         localizer={localizer}
         events={events}
+        style = {{
+          height : 1000,
+          width : 1000,
+        }}
+        selectable
+        onSelectSlot = {(e) => {
+          setModalIsOpen(true);
+          console.log(e.start.getUTCFullYear(),e.start.getMonth() + 1,e.start.getDate());
+        }}
         startAccessor="start"
         endAccessor="end"
       />
       <Modal 
+        style = {{
+          overlay : {
+            backgroundColor : "rgba(0,0,0,0.3)",
+            zIndex : 4,
+          },
+          content : {
+            height : 800,
+            width : 500,
+            left : 1100,
+          }   
+        }}
         isOpen = {ModalIsOpen}
         onRequestClose = {() => setModalIsOpen(false)}
       >
-      modal content</Modal>
+        <button onClick={() => addSchedule(2022,1,7)}>Add Schedule</button>
+      </Modal>
       </div>
 );}
 
