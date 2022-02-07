8d98be4d80fcc5b1c6a9d0ca3d2a24fc5a3551dc
alexjun12 <alexjun12@naver.com>
2022-02-07 13:12:19 +0900

    22_02_07

--- src/components/DayCallendar.js
+++ src/components/DayCallendar.js
@@ -1,54 +1,33 @@
 import React, { useState } from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import Modal from 'react-modal';
 
-//Modal.setAppElement('#cal');
 function DayCallendar(){
+  moment.locale('ko-KR');
+  const localizer = momentLocalizer(moment);
   const [ModalIsOpen, setModalIsOpen] = useState(false);
 
     const events = [
         {
-          id: 1,
           title: 'event 1',
           start: '2021-06-14T10:00:00',
           end: '2021-06-14T12:00:00',
         },
-        {
-          id: 2,
-          title: 'event 2',
-          start: '2021-06-16T13:00:00',
-          end: '2021-06-16T18:00:00',
-        },
-        { id: 3, title: 'event 3', start: '2021-06-17', end: '2021-06-20' },
     ];
-
     return(
       <div>
-        <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        headerToolbar={{
-            center: 'dayGridMonth,timeGridWeek,timeGridDay new',
-        }}
-        customButtons={{
-            new: {
-              text: 'new',
-              click: () => {},
-            },
-        }}
-        events = {events}
-        eventColor="red"
-        nowIndicator
-        dateClick={(e) => setModalIsOpen(true)}
-        eventClick={(e) => console.log(e.event.id)}
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
       />
       <Modal 
-      isOpen = {ModalIsOpen}
-      onRequestClose = {() => setModalIsOpen(false)}
+        isOpen = {ModalIsOpen}
+        onRequestClose = {() => setModalIsOpen(false)}
       >
       modal content</Modal>
       </div>
--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
