2648c120318078d3fc65c1618889e833fe330a69
alexjun12 <alexjun12@naver.com>
2022-02-12 03:23:34 +0900

    22_02_12

--- src/components/DayCallendar.js
+++ src/components/DayCallendar.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'calEdit.css';
 import "react-datepicker/dist/react-datepicker.css";
 import "style.css";
 
